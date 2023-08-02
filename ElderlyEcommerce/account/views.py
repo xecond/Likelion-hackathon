44c638212ddbfcbb455499190df70044640e9e1c
hjcheong12 <108015557+hjcheong12@users.noreply.github.com>
2023-08-02 23:11:26 +0900

    Update views.py

--- ElderlyEcommerce/account/views.py
+++ ElderlyEcommerce/account/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def login(request)
+  if request.method == 'GET':
+    return render(request, 'login.html')
+  if request.method == 'POST'
+    userid = request.POST['username']
+    userpw = request.POST['password']
+    user = auth.autheticate(request, username=userid, password=userpw)
+  if user is not None:
+    auth.login(request, user)
+    return redirect('home')
+  else:
+    return render(request, 'login.html')
+
+def logout(request)
+  auth.logout(request)
+  return redirect('home')
+
+
+def signup(request):
+  if request.method == 'GET':
+    return render(request, 'signup.html')
+
+  else:
+    userid = request.POST['username']
+    userpw = request.POST['password']
+    new_user = User.objects.create_user(username=userid, password=userpw)
+    auth.login(request, new_user)
+    return redirect('home')
