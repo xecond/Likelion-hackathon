992517ba4034fe6b54873b44d5c6d150828cfbe9
doul <doulzang@gmail.com>
2023-08-12 20:48:57 +0900

    [08.12도울]프론트엔드 밑작업 완료

--- ElderlyEcommerce/ElderlyEcommerce/settings.py
+++ ElderlyEcommerce/ElderlyEcommerce/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'ElderlyEcommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ElderlyEcommerce/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
